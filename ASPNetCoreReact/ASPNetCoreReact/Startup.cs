24d65470dd4aefaaf7f626d19a6939cf34639c86
Arno Noordover <anoordover@users.noreply.github.com>
2019-11-08 19:52:54 +0100

    MVC enabled

--- ASPNetCoreReact/ASPNetCoreReact/Startup.cs
+++ ASPNetCoreReact/ASPNetCoreReact/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace ASPNetCoreReact
@@ -15,8 +16,10 @@ namespace ASPNetCoreReact
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
+        
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
@@ -24,11 +27,14 @@ namespace ASPNetCoreReact
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseStaticFiles();
+            app.UseMvc(routes =>
+                {
+                    routes.MapRoute(
+                        name: "default",
+                        template: "{controller=Home}/{action=Index}/{id?}");
+                }                                         
+            );
         }
     }
 }
